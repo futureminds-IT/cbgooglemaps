55514952d3f4fa4678f5039beeb7646d64b9f9f2
futureminds <info@futureminds.de>
2026-09-25 03:16:17 +0000

    Unique wizard icon identifier, drop dead 12/6.2-era code
    - Icons.php: ce-default-icon -> cbgooglemaps-wizard-icon (generic identifier
      could collide with other extensions); matching change in the content
      element wizard tsconfig
    - removed unused ContentElementWizard hook (used TYPO3_version + list_type)
    - removed unused IncludeBeJavascript service (TYPO3_version, TemplateService,
      siteRelPath, old addJsFile signature - all gone since 12/13)
    - removed unused *-patched library copies; the constants reference
      mapbox-gl.js (3.31.0) and leaflet.js only

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'ce-default-icon' => [
+    // Identifier mit Extension-Namen präfixiert: generische Namen wie
+    // "ce-default-icon" können mit anderen Extensions kollidieren.
+    'cbgooglemaps-wizard-icon' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:cbgooglemaps/Resources/Public/Icons/ce_wiz.svg',
     ],
